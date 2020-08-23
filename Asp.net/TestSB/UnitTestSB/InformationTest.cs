7dcc03ad9345d36a1b4b700d0574c20418ec8246
anthonybastidas49 <anthonybastidas48@gmail.com>
2020-08-22 19:45:40 -0500

    Unit Test .NET :alien:
    Unit Test .NET

---
+++ Asp.net/TestSB/UnitTestSB/InformationTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SBAL.Model;
+using SBBL.Catalogs;
+
+namespace UnitTestSB
+{
+    [TestClass]
+    public class InformationTest
+    {
+        private InformationBL bl = new InformationBL();
+
+        /// <summary>
+        /// Exception test in business layer
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void placa_ShouldThrowException()
+        {
+            String placa = null;
+            bl.validatePlaca(placa);
+        }
+        /// <summary>
+        /// Bad plate test
+        /// </summary>
+        [TestMethod]
+        public void placa_ShouldInvalid()
+        {
+            String placa = "A";
+            Boolean result;
+            result = bl.validatePlaca(placa);
+            Assert.AreEqual(false,result);
+            placa = "ABC123A";
+            result = bl.validatePlaca(placa);
+            Assert.AreEqual(false, result);
+            placa = "ABC123";
+            result = bl.validatePlaca(placa);
+            Assert.AreEqual(false, result);
+            placa = "ABC12345";
+            result = bl.validatePlaca(placa);
+            Assert.AreEqual(false, result);
+            placa = "ABC123A";
+            result = bl.validatePlaca(placa);
+            Assert.AreEqual(false, result);
+            placa = "DBC1234";
+            result = bl.validatePlaca(placa);
+            Assert.AreEqual(false, result);
+            //VALID
+            placa = "ABC1234";
+            result = bl.validatePlaca(placa);
+            Assert.AreEqual(true, result);
+        }
+        /// <summary>
+        /// Test does not circulate
+        /// </summary>
+        [TestMethod]
+        public void placa_ShouldNoCirculate()
+        {
+            Boolean result;
+            DateTime dateTime=Convert.ToDateTime("21/08/2020 08:00");
+            Information information = new Information();
+            information.placa = "ABC1230";
+            information.lastDigit = 0;
+            information.dateTime = dateTime;
+            result=bl.canCirculate(information);
+            Assert.AreEqual(false, result);
+            dateTime = Convert.ToDateTime("21/08/2020 17:00");
+            information = new Information();
+            information.placa = "ABC1239";
+            information.lastDigit = 0;
+            information.dateTime = dateTime;
+            result = bl.canCirculate(information);
+            Assert.AreEqual(false, result);
+            dateTime = Convert.ToDateTime("20/08/2020 17:00");
+            information = new Information();
+            information.placa = "ABC1237";
+            information.lastDigit = 7;
+            information.dateTime = dateTime;
+            result = bl.canCirculate(information);
+            Assert.AreEqual(false, result);
+            dateTime = Convert.ToDateTime("19/08/2020 17:00");
+            information = new Information();
+            information.placa = "ABC1236";
+            information.lastDigit = 6;
+            information.dateTime = dateTime;
+            result = bl.canCirculate(information);
+            Assert.AreEqual(false, result);
+        }
+        /// <summary>
+        /// Test circulate
+        /// </summary>
+        [TestMethod]
+        public void placa_ShouldCirculate()
+        {
+            Boolean result;
+            DateTime dateTime = Convert.ToDateTime("21/08/2020 10:00");
+            Information information = new Information();
+            information.placa = "ABC1230";
+            information.lastDigit = 0;
+            information.dateTime = dateTime;
+            result = bl.canCirculate(information);
+            Assert.AreEqual(true, result);
+            dateTime = Convert.ToDateTime("19/08/2020 06:58");
+            information = new Information();
+            information.placa = "ABC1236";
+            information.lastDigit = 6;
+            information.dateTime = dateTime;
+            result = bl.canCirculate(information);
+            Assert.AreEqual(true, result);
+            dateTime = Convert.ToDateTime("19/08/2020 07:20");
+            information = new Information();
+            information.placa = "ABC1231";
+            information.lastDigit = 1;
+            information.dateTime = dateTime;
+            result = bl.canCirculate(information);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ Asp.net/TestSB/UnitTestSB/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSB")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8527668e-da5a-4297-b70d-9e60f856829e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
